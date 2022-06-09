868b42808841edde6c3e78fb495773afa0ab4a0d
anderson olieira da silva <anderson.os88@hotmail.com>
2022-06-09 19:28:11 -0300

    js

---
+++ TQI FULLSTACK DEVELOPER/front-end/Curso-JavaScript/Aula-13/script.js
@@ -0,0 +1,10 @@
+function changeMode() {
+    console.log('cliquei');
+}
+
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode);
