1a9755edae4c2545664f188d23671364b9d1a105
anderson olieira da silva <anderson.os88@hotmail.com>
2022-06-16 19:08:03 -0300

    back-end

---
+++ TQI FULLSTACK DEVELOPER/back-end/Collections/src/list/ExemploList.java
@@ -0,0 +1,21 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploList {
+
+    public static void main(String[] args) {
+
+        List<Double> notas = new ArrayList<>();
+        notas.add(10.0);
+        notas.add(9.5);
+        notas.add(8.5);
+
+        for (int i = 0; i< notas.size(); i++) {
+            System.out.println("nota: "+ i + " - "+ notas.get(i));
+        }
+        System.out.println(notas);
+
+    }
+}
